12a2bab0aa42bd9464ffbd929703917745aa6083
ngarukiyimana sostene <ngarukiyimanasostene@gmail.com>
2023-01-10 18:09:22 +0200

    ft:read-contact -implemented get contact by id [done]

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -6,5 +6,6 @@ const contactRoute = express.Router();
 contactRoute.post("/create",ContactController.createContact);
 contactRoute.delete("/delete/:id",ContactController.deleteContact);
 contactRoute.get("/all",ContactController.getAllContact);
+contactRoute.get("/:id",ContactController.getContact);
 
 export default contactRoute;
