1a5f7f53ddf382dec110d48974e414ad25feef49
ngarukiyimana sostene <ngarukiyimanasostene@gmail.com>
2023-01-10 17:46:19 +0200

    ft:all-contacts -implemented get all contacts functionality [done]

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -5,5 +5,6 @@ const contactRoute = express.Router();
 
 contactRoute.post("/create",ContactController.createContact);
 contactRoute.delete("/delete/:id",ContactController.deleteContact);
+contactRoute.get("/all",ContactController.getAllContact);
 
 export default contactRoute;
