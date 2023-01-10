d3b6da55bae643ce909e2936999400d14c151231
ngarukiyimana sostene <ngarukiyimanasostene@gmail.com>
2023-01-10 17:43:22 +0200

    ft:delete-contact -implemented delete contact [done]

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -10,6 +10,16 @@ class ContactController{
             res.status(401).json({error:error.message});
         }
     }
+
+    static async deleteContact(req,res){
+        try {
+            await Contact.findByIdAndDelete(req.params.id);
+            res.status(200).json({message:"contact deleted"});
+        } catch (error) {
+            res.status(404).json({error:error.message});
+        }
+    }
+
 }
 
 export default ContactController;
--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -4,5 +4,6 @@ import ContactController from "../controllers/contactController";
 const contactRoute = express.Router();
 
 contactRoute.post("/create",ContactController.createContact);
+contactRoute.delete("/delete/:id",ContactController.deleteContact);
 
 export default contactRoute;
