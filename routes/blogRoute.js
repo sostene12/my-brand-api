96fc25bcd12ce44a8e5f9d5c0709beb1b36c38dd
ngarukiyimana sostene <ngarukiyimanasostene@gmail.com>
2023-01-11 15:30:43 +0200

    ft(delete-blog): delete-blog feature
    -implement delete-blog by id
    
    [finishes #ft-delete-blog]

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -7,5 +7,6 @@ const blogRoute = express.Router();
 
 blogRoute.post("/create",upload.single("image"),BlogController.createBlog);
 blogRoute.get("/:id",BlogController.getSingleBlog);
+blogRoute.delete("/delete/:id",BlogController.deleteBlog);
 
 export default blogRoute;
