fdf6176d73e94f6736cde054497aa3dd732d39c2
ngarukiyimana sostene <ngarukiyimanasostene@gmail.com>
2023-01-12 10:31:11 +0200

    ft(read-blog): read-blog chore
    -enhance the format of response
    
    [Finishes #ft-read-blog(chore)]

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -21,9 +21,9 @@ class BlogController{
     static async getSingleBlog(req,res){
         try {
             const blog = await Blog.findById(req.params.id);
-            res.status(200).json(blog);
+            res.status(200).json({status:"success",data:blog});
         } catch (error) {
-            res.status(404).json({error:error.message});
+            res.status(500).json({error:error.message});
         }
     }
 }
